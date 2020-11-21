363383eb92bcd6ac4b33a5ff0ece90b68d271b48
Cal Schafer <schafer1@student.ubc.ca>
2020-11-20 19:49:04 -0700

    added main function for ex 6 to demo.R

--- demo.R
+++ demo.R
@@ -1,9 +1,8 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Cal Schafer
+# date: 2020-11-20
 
 "This script prints out docopt args.
 Usage: demo.R <arg1> --arg2=<arg2> [<arg2b>] [--arg3=<arg3>]
-
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
@@ -12,7 +11,19 @@ Options:
 " -> doc
 
 library(docopt)
+
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg2b)
+
+main <- function(arg1, arg2, arg3=NULL, arg4=NULL) {
+  
+    print(opt)
+    print(typeof(opt))
+    if (!is.null(opt$arg2b)) {
+        print(opt$arg2b)  
+    }
+}
+
+main(opt$arg1, opt$arg2, opt$arg3, opt$arg4)
+
+
+
