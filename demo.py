ecb0740675c6b51a49773013e2bba8dfcbb8a61d
Cal Schafer <schafer1@student.ubc.ca>
2020-11-20 21:52:39 -0700

    added def main function

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
-# author: Tiffany Timbers
-# date: 2020-01-15
+# author: Cal Schafer
+# date: 2020-11-20
 
 """This script prints out docopt args.
 Usage: docopt.py <arg1> --arg2=<arg2> [<arg2b>] [--arg3=<arg3>]
 
 Options:
-<arg>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [<arg2b>]         Takes any value (this is an optional positional argument)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
@@ -13,6 +13,14 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
-print(opt['<arg2b>'])
+
+
+def main(arg, arg2, arg2b = None, arg3 = None):
+    
+    print(opt)
+    print(type(opt))
+    if arg2b != None:
+        print(opt['<arg2b>'])
+
+if __name__ == "__main__":
+    main(opt["<arg1>"], opt["--arg2"], opt["<arg2b>"], opt["--arg3"])
